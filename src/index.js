2e86d7ae45e0c07012cdf9a3c883c1b95fd8c0d1
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-04-17 21:30:08 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const bodyParser = require("body-parser");
+
+// settings:
+app.set("port", process.env.PORT || 5000);
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+// app.set("view engine", "ejs");
+
+// middlewares:
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes:
+app.use(require("./routes/routes"));
+
+// static files:
+app.use(express.static(path.join(__dirname, "public")));
+
+// 404 not found requests:
+app.use((request, response, next) => {
+    response.status(404).send("Error 404!");
+});
+
+// listening the server:
+server.listen(app.get("port"), () => {
+    console.log("Server running on port ", app.get("port"));
+});
